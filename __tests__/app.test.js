5e15b5b036d59b349b94dfd1f026b6898522ad85
Benatovich <sambenatovich@gmail.com>
2022-03-16 19:36:34 -0400

    both get tests are written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Rat = require('../lib/models/Rat');
 
 describe('Ratalog routes', () => {
   beforeEach(() => {
@@ -24,7 +25,31 @@ describe('Ratalog routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       name: 'lab rat',
+      sex: 'male',
       color: 'white',
     });
   });
+
+  it('gets a list of rats', async () => {
+    const res = await request(app)
+      .get('/api/v1/rats');
+
+    expect(res.body).toEqual(
+      await Rat.findAll()
+    );
+  });
+  
+  it('gets a rat by id', async () => {
+    const expected = await Rat.findById(1);
+    const res = await request(app)
+      .get(`/api/v1/rats/${expected.id}`);
+
+    expect(res.body).toEqual({
+      ...expected
+    });
+  });
+
+  
+
+
 });
